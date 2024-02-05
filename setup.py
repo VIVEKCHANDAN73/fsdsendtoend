4ed7bd57c4b8353f448e395c60891a7057b5a194
VIVEKCHANDAN73 <vivekchandan73@gmail.com>
2024-02-05 23:32:53 +0530

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='DimondPricePrediction',
+    version='0.0.1',
+    author='sunny savita',
+    author_email='sunny.savita@ineuron.ai',
+    packages=find_packages()
+)
