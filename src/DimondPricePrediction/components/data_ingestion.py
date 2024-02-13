4d8e7858e31771900ad098b103e3bbde6afeb692
VIVEKCHANDAN73 <vivekchandan73@gmail.com>
2024-02-13 17:46:35 +0530

    added

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -38,6 +38,12 @@ class DataIngestion:
 
             logging.info("data ingestion part completed")
 
+            return(
+
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
         except Exception as e:
             logging.info("exception during occured at ingestion stage")
             raise customexception(e,sys)
