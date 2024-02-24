8156a0f21c0a0b63da487c88eb3b35bc8c052032
VIVEKCHANDAN73 <vivekchandan73@gmail.com>
2024-02-24 16:17:04 +0530

    training successful

--- src/DimondPricePrediction/components/model_trainer.py
+++ src/DimondPricePrediction/components/model_trainer.py
@@ -9,6 +9,7 @@ from src.DimondPricePrediction.logger import logging
 from src.DimondPricePrediction.utils.utils import save_object
 from src.DimondPricePrediction.utils.utils import evaluate_model
 
+
 from sklearn.linear_model import LinearRegression,Ridge,Lasso,ElasticNet
 
 
